c9345115dec75813d83664a8f56c9499954ec03e
Caste95 <riccardo.castelletto@gmail.com>
2019-09-19 12:43:46 +0200

    update to handle all possible dimensions

--- results/results_plot.m
+++ results/results_plot.m
@@ -1,18 +1,41 @@
 clearvars;
 clc; 
 
-m = readtable('dt2.csv');
+m = readtable('rt50sec50veh.csv');
 time = table2array(unique(m(:,1)));
-n_v = size(m, 1)/19; %size(time, 1)
-tensor = zeros(n_v, size(m,2) - 1, size(time, 1));
+thr = zeros(size(time, 1), 1);
+thr_var = zeros(size(time, 1), 1);
+thr_s = zeros(1, 1);
 
-for i = 1:size(time)
-   tensor(:, :, i) = table2array(m(((i-1)*n_v+1):n_v*i, 2:size(m, 2)));
+delay = zeros(size(time, 1), 1);
+delay_s = zeros(1, 1);
+first = true;
+
+for i=1:size(time, 1)
+    first = true;
+    for j=1:size(m, 1)
+        if time(i, :) == m{j, 1}
+            if first
+                thr_s(1) = m{j, 3};
+                delay_s(1) = m{j, 6};
+                first = false;
+            else
+                delay_s = [delay_s m{j, 6}];
+                thr_s = [thr_s m{j, 3}];
+            end
+        end
+    end
+    thr(i, 1) = mean(thr_s);
+    thr_var(i, 1) = var(thr_s);
+    delay(i, 1) = mean(delay_s);
+    
+    thr_s = zeros(1, 1);
+    delay_s = zeros(1, 1);
 end
-disp(tensor)
 
-thr = mean(tensor(:, 2, :));
-vars_thr = var(tensor(:, 2, :));
 
-errorbar(time(:, 1), squeeze(thr), squeeze(vars_thr));
 
+figure;
+errorbar(time(:, 1), thr, thr_var);
+figure;
+plot(time(:, 1), delay, 'r');
