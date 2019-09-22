2e52a14c29930e3407481ae2416b694cf7a497e0
Caste95 <riccardo.castelletto@gmail.com>
2019-09-22 17:02:59 +0200

    completed program

--- results/results_compare.m
+++ results/results_compare.m
@@ -2,16 +2,22 @@
 clearvars;
 clc; 
 
-filename = "50-50-nop";
-filename2 = "50-50-pr";
-filename3 = "50-50-sir";
-filename4 = "50-50-nor";
+filename = "100veh-standard-standard-standard";
+filename2 = "100veh-hard_1_5-soft_1_2-time_1_5";
+filename3 = "100veh-hard_2_3-soft_1_8-time_2_3";
+%filename4 = "100veh-false-false";
+%filename5 = "100veh-window_8";
+% filename6 = "175veh-all_standard";
+% filename7 = "200veh-all_standard";
 
 %reading csv file and taking the time intervals involved
 m = readtable(filename + '.csv');
 m2 = readtable(filename2 + '.csv');
 m3 = readtable(filename3 + '.csv');
-m4 = readtable(filename4 + '.csv');
+%m4 = readtable(filename4 + '.csv');
+%m5 = readtable(filename5 + '.csv');
+% m6 = readtable(filename6 + '.csv');
+% m7 = readtable(filename7 + '.csv');
 
 time = table2array(unique(m(:,1)));
 
@@ -23,8 +29,14 @@ thr2 = zeros(size(time, 1), 1);
 thr_s2 = zeros(1, 1);
 thr3 = zeros(size(time, 1), 1);
 thr_s3 = zeros(1, 1);
-thr4 = zeros(size(time, 1), 1);
-thr_s4 = zeros(1, 1);
+% thr4 = zeros(size(time, 1), 1);
+% thr_s4 = zeros(1, 1);
+% thr5 = zeros(size(time, 1), 1);
+% thr_s5 = zeros(1, 1);
+% thr6 = zeros(size(time, 1), 1);
+% thr_s6 = zeros(1, 1);
+% thr7 = zeros(size(time, 1), 1);
+% thr_s7 = zeros(1, 1);
 
 %arrays for mean delay calculation
 delay = zeros(size(time, 1), 1);
@@ -33,8 +45,14 @@ delay2 = zeros(size(time, 1), 1);
 delay_s2 = zeros(1, 1);
 delay3 = zeros(size(time, 1), 1);
 delay_s3 = zeros(1, 1);
-delay4 = zeros(size(time, 1), 1);
-delay_s4 = zeros(1, 1);
+% delay4 = zeros(size(time, 1), 1);
+% delay_s4 = zeros(1, 1);
+% delay5 = zeros(size(time, 1), 1);
+% delay_s5 = zeros(1, 1);
+% delay6 = zeros(size(time, 1), 1);
+% delay_s6 = zeros(1, 1);
+% delay7 = zeros(size(time, 1), 1);
+% delay_s7 = zeros(1, 1);
 
 %arrays for mean packet loss calculation
 pl = zeros(size(time, 1), 1);
@@ -43,8 +61,14 @@ pl2 = zeros(size(time, 1), 1);
 pl_s2 = zeros(1, 1);
 pl3 = zeros(size(time, 1), 1);
 pl_s3 = zeros(1, 1);
-pl4 = zeros(size(time, 1), 1);
-pl_s4 = zeros(1, 1);
+% pl4 = zeros(size(time, 1), 1);
+% pl_s4 = zeros(1, 1);
+% pl5 = zeros(size(time, 1), 1);
+% pl_s5 = zeros(1, 1);
+% pl6 = zeros(size(time, 1), 1);
+% pl_s6 = zeros(1, 1);
+% pl7 = zeros(size(time, 1), 1);
+% pl_s7 = zeros(1, 1);
 
 first = true;   
 for i=1:size(time, 1)
@@ -151,84 +175,343 @@ end
 
 disp("Third done");
 
-first = true;   
-for i=1:size(time, 1)
-    first = true;
-    for j=1:size(m4, 1)
-        if time(i, :) == m4{j, 1}
-            if first
-                %saving stats
-                thr_s4(1) = m4{j, 3};             %thr
-                delay_s4(1) = m4{j, 6};           %delay
-                if m4{j, 13} ~= -1           
-                    pl_s4 = m4{j, 13};            %if there are packets, packet loss
-                end
-                first = false;
-            else
-                delay_s4 = [delay_s4 m4{j, 6}];    %same here
-                thr_s4 = [thr_s4 m4{j, 3}];
-                if m4{j, 13} ~= -1
-                    pl_s4 = [pl_s4 m4{j, 13}];
-                end
-            end
-        end
-    end
-    %calculating stats per time instant
-    thr4(i, 1) = mean(thr_s4);
-    delay4(i, 1) = mean(delay_s4);
-    pl4(i, 1) = mean(pl_s4);
-    
-    %zeroing temporary arrays for further analysis
-    thr_s4 = zeros(1, 1);
-    delay_s4 = zeros(1, 1);
-    pl_s4 = zeros(1, 1);
-end
+% first = true;   
+% for i=1:size(time, 1)
+%     first = true;
+%     for j=1:size(m4, 1)
+%         if time(i, :) == m4{j, 1}
+%             if first
+%                 %saving stats
+%                 thr_s4(1) = m4{j, 3};             %thr
+%                 delay_s4(1) = m4{j, 6};           %delay
+%                 if m4{j, 13} ~= -1           
+%                     pl_s4 = m4{j, 13};            %if there are packets, packet loss
+%                 end
+%                 first = false;
+%             else
+%                 delay_s4 = [delay_s4 m4{j, 6}];    %same here
+%                 thr_s4 = [thr_s4 m4{j, 3}];
+%                 if m4{j, 13} ~= -1
+%                     pl_s4 = [pl_s4 m4{j, 13}];
+%                 end
+%             end
+%         end
+%     end
+%     %calculating stats per time instant
+%     thr4(i, 1) = mean(thr_s4);
+%     delay4(i, 1) = mean(delay_s4);
+%     pl4(i, 1) = mean(pl_s4);
+%     
+%     %zeroing temporary arrays for further analysis
+%     thr_s4 = zeros(1, 1);
+%     delay_s4 = zeros(1, 1);
+%     pl_s4 = zeros(1, 1);
+% end
+% 
+% disp("Fourth done");
 
-disp("Fourth done");
+% first = true;   
+% for i=1:size(time, 1)
+%     first = true;
+%     for j=1:size(m5, 1)
+%         if time(i, :) == m5{j, 1}
+%             if first
+%                 %saving stats
+%                 thr_s5(1) = m5{j, 3};             %thr
+%                 delay_s5(1) = m5{j, 6};           %delay
+%                 if m5{j, 13} ~= -1           
+%                     pl_s5 = m5{j, 13};            %if there are packets, packet loss
+%                 end
+%                 first = false;
+%             else
+%                 delay_s5 = [delay_s5 m5{j, 6}];    %same here
+%                 thr_s5 = [thr_s5 m5{j, 3}];
+%                 if m5{j, 13} ~= -1
+%                     pl_s5 = [pl_s5 m5{j, 13}];
+%                 end
+%             end
+%         end
+%     end
+%     %calculating stats per time instant
+%     thr5(i, 1) = mean(thr_s5);
+%     delay5(i, 1) = mean(delay_s5);
+%     pl5(i, 1) = mean(pl_s5);
+%     
+%     %zeroing temporary arrays for further analysis
+%     thr_s5 = zeros(1, 1);
+%     delay_s5 = zeros(1, 1);
+%     pl_s5 = zeros(1, 1);
+% end
+% 
+% disp("Fifth done");
 
-%% PLOTS
+% first = true;   
+% for i=1:size(time, 1)
+%     first = true;
+%     for j=1:size(m6, 1)
+%         if time(i, :) == m6{j, 1}
+%             if first
+%                 %saving stats
+%                 thr_s6(1) = m6{j, 3};             %thr
+%                 delay_s6(1) = m6{j, 6};           %delay
+%                 if m6{j, 13} ~= -1           
+%                     pl_s6 = m6{j, 13};            %if there are packets, packet loss
+%                 end
+%                 first = false;
+%             else
+%                 delay_s6 = [delay_s6 m6{j, 6}];    %same here
+%                 thr_s6 = [thr_s6 m6{j, 3}];
+%                 if m6{j, 13} ~= -1
+%                     pl_s6 = [pl_s6 m6{j, 13}];
+%                 end
+%             end
+%         end
+%     end
+%     %calculating stats per time instant
+%     thr6(i, 1) = mean(thr_s6);
+%     delay6(i, 1) = mean(delay_s6);
+%     pl6(i, 1) = mean(pl_s6);
+%     
+%     %zeroing temporary arrays for further analysis
+%     thr_s6 = zeros(1, 1);
+%     delay_s6 = zeros(1, 1);
+%     pl_s6 = zeros(1, 1);
+% end
+% 
+% disp("Sixth done");
+% 
+% first = true;   
+% for i=1:size(time, 1)
+%     first = true;
+%     for j=1:size(m7, 1)
+%         if time(i, :) == m7{j, 1}
+%             if first
+%                 %saving stats
+%                 thr_s7(1) = m7{j, 3};             %thr
+%                 delay_s7(1) = m7{j, 6};           %delay
+%                 if m7{j, 13} ~= -1           
+%                     pl_s7 = m7{j, 13};            %if there are packets, packet loss
+%                 end
+%                 first = false;
+%             else
+%                 delay_s7 = [delay_s7 m7{j, 6}];    %same here
+%                 thr_s7 = [thr_s7 m7{j, 3}];
+%                 if m7{j, 13} ~= -1
+%                     pl_s7 = [pl_s7 m7{j, 13}];
+%                 end
+%             end
+%         end
+%     end
+%     %calculating stats per time instant
+%     thr7(i, 1) = mean(thr_s7);
+%     delay7(i, 1) = mean(delay_s7);
+%     pl7(i, 1) = mean(pl_s7);
+%     
+%     %zeroing temporary arrays for further analysis
+%     thr_s7 = zeros(1, 1);
+%     delay_s7 = zeros(1, 1);
+%     pl_s7 = zeros(1, 1);
+% end
+% 
+% disp("Seventh done");
+
+%% PLOTS FOR WIN SIZE
+
+save('thr-WS.mat', 'thr');
+save('thr2-WS.mat', 'thr2');
+save('thr3-WS.mat', 'thr3');
+save('thr4-WS.mat', 'thr4');
+save('thr5-WS.mat', 'thr5');
+save('delay-WS.mat', 'delay');
+save('delay2-WS.mat', 'delay2');
+save('delay3-WS.mat', 'delay3');
+save('delay4-WS.mat', 'delay4');
+save('delay5-WS.mat', 'delay5');
+save('pl-WS.mat', 'pl');
+save('pl2-WS.mat', 'pl2');
+save('pl3-WS.mat', 'pl3');
+save('pl4-WS.mat', 'pl4');
+save('pl5-WS.mat', 'pl5');
 
 %plots and image savings
-totalfig = figure;
-%fig = figure;
-subplot(3, 1, 1);
+%totalfig = figure;
+fig = figure;
 plot(time(:, 1), thr);
 hold on;
 plot(time(:, 1), thr2, 'r');
 plot(time(:, 1), thr3, 'g');
 plot(time(:, 1), thr4, 'k');
-title('Comparison throughput');
+plot(time(:, 1), thr5, 'm');
+title('Mean throughput per window size');
 ylabel('Throughput - Mbps'); 
 xlabel('Time - s'); 
 grid on;
-%legend({'Baseline', 'Project', 'Only rerouting', 'Only interval change'}, 'Location', "South", 'NumColumns', 2);
-%saveas(fig, "Compare throughput per time " + filename + " " + filename2 + " " + filename3 + " " + filename4 + ".jpg");
+legend({'w: 1', 'w: 2', 'w: 3', 'w: 4', 'w: 8'}, 'Location', "South", 'NumColumns', 2);
+saveas(fig, "Compare throughput per window size.jpg");
 
-%fig2 = figure;
-subplot(3, 1, 2);
+fig2 = figure;
 plot(time(:, 1), delay);
 hold on;
 plot(time(:, 1), delay2, 'r');
 plot(time(:, 1), delay3, 'g');
 plot(time(:, 1), delay4, 'k');
-title('Comparison delay');
+plot(time(:, 1), delay5, 'm');
+title('Mean delay per window size');
 ylabel('Delay - us'); 
 xlabel('Time - s'); 
 grid on;
-legend({'Baseline', 'Project', 'Only rerouting', 'Only interval change'}, 'Location', "NorthEast", 'NumColumns', 2);
-%saveas(fig2, "Compare delay per time " + filename + " " + filename2 + " " + filename3 + " " + filename4 + ".jpg");
+legend({'w: 1', 'w: 2', 'w: 3', 'w: 4', 'w: 8'}, 'Location', "South", 'NumColumns', 2);
+saveas(fig2, "Compare delay per window size.jpg");
 
-%fig3 = figure;
-subplot(3, 1, 3);
+fig3 = figure;
 plot(time(:, 1), pl);
 hold on;
 plot(time(:, 1), pl2, 'r');
 plot(time(:, 1), pl3, 'g');
 plot(time(:, 1), pl4, 'k');
-title('Comparison packet loss');
+plot(time(:, 1), pl5, 'm');
+title('Mean packet loss per window size');
 ylabel('Packet loss - rate'); 
 xlabel('Time - s'); 
 grid on;
-%legend({'Baseline', 'Project', 'Only rerouting', 'Only interval change'}, 'Location', "South", 'NumColumns', 2);
-%saveas(fig3, "Compare packet loss per time " + filename + " " + filename2 + " " + filename3 + " " + filename4 + ".jpg");
-saveas(totalfig, "Compare packet loss per time " + filename + " " + filename2 + " " + filename3 + " " + filename4 + " TOTAL.jpg");
+legend({'w: 1', 'w: 2', 'w: 3', 'w: 4', 'w: 8'}, 'Location', "South", 'NumColumns', 2);
+saveas(fig3, "Compare packet loss per window size.jpg");
+%saveas(totalfig, "Compare packet loss per time " + filename + " " + filename2 + " " + filename3 + " " + filename4 + " TOTAL.jpg");
+
+%% PLOT FOR HUGE COMPARE
+load('Tthr.mat');
+load('Tdelay.mat');
+load('Tpl.mat');
+%Tthr = [mean(thr) mean(thr2) mean(thr3) mean(thr4) mean(thr5) mean(thr6) mean(thr7)];
+%Tdelay = [mean(delay) mean(delay2) mean(delay3) mean(delay4) mean(delay5) mean(delay6) mean(delay7)];
+%Tpl = [mean(pl) mean(pl2) mean(pl3) mean(pl4) mean(pl5) mean(pl6) mean(pl7)];
+save('Tthr.mat', 'Tthr');
+save('Tdelay.mat', 'Tdelay');
+save('Tpl.mat', 'Tpl');
+
+x = [50 75 100 125 150 175 200];
+
+fig = figure;
+plot(x, Tthr);
+title('Mean throughput vs # of vehicles');
+ylabel('Throughput - Mbps'); 
+xlabel('# of vehicles'); 
+grid minor;
+saveas(fig, "Mean throughput vs # of vehicles.jpg");
+
+fig2 = figure;
+plot(x, Tdelay, 'r');
+title('Mean delay vs # of vehicles');
+ylabel('Delay - us'); 
+xlabel('# of vehicles'); 
+grid minor;
+saveas(fig2, "Mean delay vs # of vehicles.jpg");
+
+fig3 = figure;
+plot(x, Tpl, 'g');
+title('Mean packet loss vs # of vehicles');
+ylabel('Packet loss - Rate'); 
+xlabel('# of vehicles'); 
+grid minor;
+saveas(fig3, "Mean packet loss vs # of vehicles.jpg");
+
+%% PLOTS FOR TT-TF-FT-FF
+% save('thr-TTFF.mat', 'thr');
+% save('thr2-TTFF.mat', 'thr2');
+% save('thr3-TTFF.mat', 'thr3');
+% save('thr4-TTFF.mat', 'thr4');
+% save('delay-TTFF.mat', 'delay');
+% save('delay2-TTFF.mat', 'delay2');
+% save('delay3-TTFF.mat', 'delay3');
+% save('delay4-TTFF.mat', 'delay4');
+% save('pl-TTFF.mat', 'pl');
+% save('pl2-TTFF.mat', 'pl2');
+% save('pl3-TTFF.mat', 'pl3');
+% save('pl4-TTFF.mat', 'pl4');
+
+TTFFthr = [mean(thr) mean(thr2) mean(thr3) mean(thr4)];
+TTFFdelay = [mean(delay) mean(delay2) mean(delay3) mean(delay4)];
+TTFFpl = [mean(pl) mean(pl2) mean(pl3) mean(pl4)];
+save('TTFFthr.mat', 'TTFFthr');
+save('TTFFdelay.mat', 'TTFFdelay');
+save('TTFFpl.mat', 'TTFFpl');
+
+x = ['TT'; 'TF'; 'FT'; 'FF'];
+
+fig = figure;
+bar(TTFFthr);
+set(gca,'xticklabel', x);
+title('Mean throughput vs project');
+ylabel('Throughput - Mbps'); 
+xlabel('Combination of strategies'); 
+grid minor;
+saveas(fig, "Mean throughput vs project.jpg");
+
+fig2 = figure;
+bar(TTFFdelay, 'r');
+set(gca,'xticklabel', x);
+title('Mean delay vs project');
+ylabel('Delay - us'); 
+xlabel('Combination of strategies'); 
+grid minor;
+saveas(fig2, "Mean delay vs project.jpg");
+
+fig3 = figure;
+bar(TTFFpl, 'g');
+set(gca,'xticklabel', x);
+title('Mean packet loss vs project');
+ylabel('Packet loss - Rate'); 
+xlabel('Combination of strategies'); 
+xTicks = ["TT"; "TF"; "FT"; "FF"];
+grid minor;
+saveas(fig3, "Mean packet loss vs project.jpg");
+
+%% PLOTS FOR THR AGGRESSIVENESS
+
+save('thr-TH.mat', 'thr');
+save('thr2-TH.mat', 'thr2');
+save('thr3-TH.mat', 'thr3');
+save('delay-TH.mat', 'delay');
+save('delay2-TH.mat', 'delay2');
+save('delay3-TH.mat', 'delay3');
+save('pl-TH.mat', 'pl');
+save('pl2-TH.mat', 'pl2');
+save('pl3-TH.mat', 'pl3');
+
+%plots and image savings
+fig = figure;
+plot(time(:, 1), thr);
+hold on;
+plot(time(:, 1), thr2, 'r');
+plot(time(:, 1), thr3, 'g');
+title('Mean throughput vs aggressiveness');
+ylabel('Throughput - Mbps'); 
+xlabel('Time - s'); 
+grid on;
+legend({'Standard', 'Aggressive', 'Weak'}, 'Location', "South", 'NumColumns', 2);
+saveas(fig, "Compare throughput vs aggressiveness on threshold.jpg");
+
+fig2 = figure;
+plot(time(:, 1), delay);
+hold on;
+plot(time(:, 1), delay2, 'r');
+plot(time(:, 1), delay3, 'g');
+title('Mean delay vs aggressiveness');
+ylabel('Delay - us'); 
+xlabel('Time - s'); 
+grid on;
+legend({'Standard', 'Aggressive', 'Weak'}, 'Location', "South", 'NumColumns', 2);
+saveas(fig2, "Compare delay vs aggressiveness on threshold.jpg");
+
+fig3 = figure;
+plot(time(:, 1), pl);
+hold on;
+plot(time(:, 1), pl2, 'r');
+plot(time(:, 1), pl3, 'g');
+title('Mean packet loss vs aggressiveness');
+ylabel('Packet loss - rate'); 
+xlabel('Time - s'); 
+grid on;
+legend({'Standard', 'Aggressive', 'Weak'}, 'Location', "NorthEast", 'NumColumns', 2);
+saveas(fig3, "Compare packet loss vs aggressiveness on threshold.jpg");
+
