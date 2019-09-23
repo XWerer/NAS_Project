ba7011aea7885c1a5ec7bda2c4d5447289044c60
XWerer <dadomw2@gmail.com>
2019-09-23 15:58:29 +0200

    other results

--- script_3.sh
+++ script_3.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 
-./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=50veh-false-false --MaxVehicles=50 --Project1=False --Project2=False"
-./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=150veh-false-false --MaxVehicles=150 --Project1=False --Project2=False"
 ./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=200veh-false-false --MaxVehicles=200 --Project1=False --Project2=False"
 
 ./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=150veh-true-false --MaxVehicles=150 --Project1=True --Project2=False"
 ./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=200veh-true-false --MaxVehicles=200 --Project1=True --Project2=False"
 
-./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=150veh-false-true --MaxVehicles=150 --Project1=False --Project2=True"
 ./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=200veh-false-true --MaxVehicles=200 --Project1=False --Project2=True"
 
 
