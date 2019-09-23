20fe3031c22f1dcc53723824db16634caf4ee59b
XWerer <dadomw2@gmail.com>
2019-09-23 22:57:18 +0200

    one more result

--- script_3.sh
+++ script_3.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-
-./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=200veh-false-false --MaxVehicles=200 --Project1=False --Project2=False"
-
-./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=150veh-true-false --MaxVehicles=150 --Project1=True --Project2=False"
 ./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=200veh-true-false --MaxVehicles=200 --Project1=True --Project2=False"
-
-./waf --run "scratch/project-test --Interval=0.007 --Window=0.5 --PacketSize=1000 --Filename=200veh-false-true --MaxVehicles=200 --Project1=False --Project2=True"
 
 
 
